fd40a2fc6acf4785c67f2d6bdb0c01c607816ffc
Rithvik Alluri <34564310+ra3738@users.noreply.github.com>
2019-05-08 14:17:37 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Tutorial
+
 // class Square extends React.Component {
 //   constructor(props) {
 //     super(props);
